cb2b70099e943eda6b536b571bd871d4e8bdd059
Suraj Mandal <surajmandalcell@gmail.com>
2023-04-30 23:42:40 -0400

    - Successfully ping two random urls in constant intervals - Dont think the logging works tho - Added proper .gitignore for a rust project - Added cargo to the project - Removed `run.sh` as it is no longer required

--- main.rs
+++ main.rs
@@ -1,3 +1,37 @@
+use std::{thread, time};
+use rand::seq::SliceRandom;
+use reqwest::blocking::Client;
+use core::time::Duration;
+
+struct Config {
+    domains: Vec<&'static str>,
+    interval_secs: u64,
+    timeout_secs: u64,
+}
+
+fn ping_domains(config: Config) {
+    let client = Client::builder()
+        .timeout(Duration::from_secs(config.timeout_secs))
+        .build()
+        .unwrap();
+
+    loop {
+        let domain = config.domains.choose(&mut rand::thread_rng()).unwrap();
+
+        match client.get(*domain).send() {
+            Ok(res) => println!("Pinged {}: {:?}", domain, res.status()),
+            Err(e) => println!("Failed to ping {}: {}", domain, e),
+        }
+
+        thread::sleep(time::Duration::from_secs(config.interval_secs));
+    }
+}
+
+
 fn main() {
-    print!("Initial commit");
+   ping_domains(Config {
+        domains: vec!["https://www.google.com", "https://www.bing.com"],
+        interval_secs: 1,
+        timeout_secs: 5,
+    })
 }
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-name=$(basename $1 .rs)
-rustc $@ && ./$name && rm $name
